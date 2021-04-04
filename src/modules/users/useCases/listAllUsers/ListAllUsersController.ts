0503731e78e6b6b4d9c8041cf012896e236de410
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-04-03 23:20:29 -0300

    refactor: adapt http header definition

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from 'express';
+import { IncomingHttpHeaders } from 'http';
 
 import { ListAllUsersUseCase } from './ListAllUsersUseCase';
+
+interface ICustomHeader {
+  user_id: string;
+}
+
+interface IReqCustom<THeader> extends Request {
+  headers: IncomingHttpHeaders & THeader;
+}
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    const user_id = request.headers.user_id as string;
+  handle(request: IReqCustom<ICustomHeader>, response: Response): Response {
+    const { user_id } = request.headers;
 
     try {
       const users = this.listAllUsersUseCase.execute({ user_id });
