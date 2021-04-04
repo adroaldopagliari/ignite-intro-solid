a7ca77b89bb7c9eded9781e91a2a6dace76b9e71
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-04-04 00:09:03 -0300

    refactor: remove console logs

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,8 +8,6 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const user_id = request.headers.user_id as string;
 
-    console.log(user_id);
-
     try {
       const users = this.listAllUsersUseCase.execute({ user_id });
       return response.status(200).json(users);
